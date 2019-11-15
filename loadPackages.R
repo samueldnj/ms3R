418fe9b26e20698e90b2feb9753e56842bbaf00a
Samuel Johnson <samueldnjohnson@gmail.com>
2019-11-15 10:26:20 -0800

    Adding history

---
+++ loadPackages.R
@@ -0,0 +1,34 @@
+# <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+# loadPackages.R
+# 
+# Checks if required packages are installed. If not, installs them.
+# Then loads all required packages.
+# 
+# 
+# <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+cranPackages <- c("coda",
+                  "tidyverse",
+                  "TMB",
+                  "RColorBrewer",
+                  "parallel",
+                  "stringr",
+                  "wesanderson",
+                  "scales",
+                  "beepr",
+                  "tmbstan",
+                  "here",
+                  "bookdown",
+                  "kableExtra" )
+
+for( pkg in cranPackages )
+  while(!require(pkg, character.only = TRUE) )
+    install.packages( pkg, repos = "https://mirror.its.sfu.ca/mirror/CRAN/" )
+
+
+githubPackages <- c(ggsidekick = "seananderson/ggsidekick",
+                    csasdown = "pbs-assess/csasdown" )
+
+for( pkgIdx in 1:length(githubPackages) )
+  while(!require(names(githubPackages)[pkgIdx], character.only = TRUE))
+    devtools::install_github(githubPackages[pkgIdx])
