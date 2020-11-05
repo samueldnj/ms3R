b4d09aa6e779bd89fe8f297115f7475c8957dc78
Samuel D N Johnson <samueldnjohnson@gmail.com>
2020-11-04 21:33:54 -0800

    Updated results

--- docs/rebuildingPlan/renderDoc.R
+++ docs/rebuildingPlan/renderDoc.R
@@ -7,6 +7,14 @@ library(bookdown)
 # setwd(here::here('docs/rebuildingPlan'))
 
 # csasdown::draft("sr") # creates Science Response Template in working dir
+# PDF full paper
 bookdown::render_book(input = "index.Rmd", clean = TRUE, output_format = "csasdown::sr_pdf", config_file = "_bookdown.yml")
+
+# docx body
 bookdown::render_book(input = "index.Rmd", clean = TRUE, output_format = "csasdown::sr_word", config_file = "_bookdown_docx.yml")
 
+# pdf tandF
+bookdown::render_book(input = "index.Rmd", clean = TRUE, output_format = "csasdown::sr_pdf", config_file = "_bookdown_tandf.yml")
+
+# docx tandF
+bookdown::render_book(input = "index.Rmd", clean = TRUE, output_format = "csasdown::sr_word", config_file = "_bookdown_tandf.yml")
